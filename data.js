6f57768dee7a3931810a8c79d47461c77ea9e89e
Andrei-360-tour <156935416+Andrei-360-tour@users.noreply.github.com>
2024-06-04 16:05:22 +0300

    Commit

---
+++ data.js
@@ -0,0 +1,771 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-bh-1_overview-1",
+      "name": "BH-1_Overview 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.8917013505049916,
+        "pitch": 0.319283763314715,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8817902541164919,
+          "pitch": 0.20679173419046393,
+          "rotation": 0,
+          "target": "1-bh-1_overview-2"
+        },
+        {
+          "yaw": -1.0117397421706507,
+          "pitch": 0.39465195430733857,
+          "rotation": 0,
+          "target": "2-bh-1_well-head-s"
+        },
+        {
+          "yaw": -1.037947177769798,
+          "pitch": 0.17612583935920156,
+          "rotation": 0,
+          "target": "11-bh-1_cocrete-pad"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-bh-1_overview-2",
+      "name": "BH-1_Overview 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.340573353155758,
+        "pitch": 0.3553894033404852,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3353267411066696,
+          "pitch": 0.008778526109928464,
+          "rotation": 0,
+          "target": "0-bh-1_overview-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-bh-1_well-head-s",
+      "name": "BH-1_Well Head S",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -0.05479521771069429,
+        "pitch": 0.3924332499427585,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2232685929547458,
+          "pitch": -0.25573028707055556,
+          "rotation": 0,
+          "target": "0-bh-1_overview-1"
+        },
+        {
+          "yaw": -1.2010132936972457,
+          "pitch": 0.2319151367652541,
+          "rotation": 0,
+          "target": "3-bh-1_well-head-w"
+        },
+        {
+          "yaw": 2.2484550846396,
+          "pitch": 0.07762426954862534,
+          "rotation": 0,
+          "target": "14-bh-1_well-pad-7"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-bh-1_well-head-w",
+      "name": "BH-1_Well Head W",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 0.23772694452952692,
+        "pitch": 0.3021388738497137,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7185783441402442,
+          "pitch": 0.4011054146826236,
+          "rotation": 0,
+          "target": "2-bh-1_well-head-s"
+        },
+        {
+          "yaw": -0.7219283782432981,
+          "pitch": 0.2851125192776216,
+          "rotation": 0,
+          "target": "4-bh-1_well-head-n"
+        },
+        {
+          "yaw": 2.9974825508883294,
+          "pitch": 0.10619252279082758,
+          "rotation": 0,
+          "target": "11-bh-1_cocrete-pad"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-bh-1_well-head-n",
+      "name": "BH-1_Well Head N",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 0.13909555265024665,
+        "pitch": 0.3658076523961711,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3090963567942744,
+          "pitch": 0.31139737431109005,
+          "rotation": 0,
+          "target": "3-bh-1_well-head-w"
+        },
+        {
+          "yaw": -1.2875429931341564,
+          "pitch": 0.11754956851950382,
+          "rotation": 0,
+          "target": "5-bh-1_well-head-e"
+        },
+        {
+          "yaw": 3.1303807127653682,
+          "pitch": 0.21610006259793657,
+          "rotation": 0,
+          "target": "6-bh-1_well-pad"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-bh-1_well-head-e",
+      "name": "BH-1_Well Head E",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -0.2925232530951245,
+        "pitch": 0.495461448305317,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6625519643276903,
+          "pitch": 0.24888472260494687,
+          "rotation": 0,
+          "target": "4-bh-1_well-head-n"
+        },
+        {
+          "yaw": -0.8526729304790805,
+          "pitch": 0.3362047156999921,
+          "rotation": 0,
+          "target": "2-bh-1_well-head-s"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-bh-1_well-pad",
+      "name": "BH-1_Well Pad",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -2.93211439555637,
+        "pitch": 0.29097278070218735,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9379765222996,
+          "pitch": 0.3132768610248853,
+          "rotation": 0,
+          "target": "4-bh-1_well-head-n"
+        },
+        {
+          "yaw": 0.0738948520621392,
+          "pitch": 0.1336843647594339,
+          "rotation": 0,
+          "target": "7-bh-1_well-pad-1"
+        },
+        {
+          "yaw": -1.0773140619866126,
+          "pitch": 0.1423552492691229,
+          "rotation": 0,
+          "target": "8-bh-1_well-pad-2"
+        },
+        {
+          "yaw": 2.2501535395805163,
+          "pitch": 0.11379576836099403,
+          "rotation": 0,
+          "target": "12-bh-1_well-pad-5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-bh-1_well-pad-1",
+      "name": "BH-1_Well Pad 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 2.9364934329153147,
+        "pitch": 0.16001944741473295,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8403408573197675,
+          "pitch": 0.09413412581904623,
+          "rotation": 0,
+          "target": "6-bh-1_well-pad"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-bh-1_well-pad-2",
+      "name": "BH-1_Well Pad 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -2.1999370050074702,
+        "pitch": 0.1808341317542208,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.6566440113078045,
+          "pitch": 0.13423142992212256,
+          "rotation": 0,
+          "target": "6-bh-1_well-pad"
+        },
+        {
+          "yaw": 0.5662656400737767,
+          "pitch": -0.009485837810569109,
+          "rotation": 0,
+          "target": "9-bh-1_well-pad-3"
+        },
+        {
+          "yaw": -1.206138122589934,
+          "pitch": 0.09385538529441462,
+          "rotation": 0,
+          "target": "11-bh-1_cocrete-pad"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-bh-1_well-pad-3",
+      "name": "BH-1_Well Pad 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -1.8965522696234167,
+        "pitch": 0.08269835832032868,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3336824174084363,
+          "pitch": 0.10531809770995437,
+          "rotation": 0,
+          "target": "8-bh-1_well-pad-2"
+        },
+        {
+          "yaw": -1.6643029986759394,
+          "pitch": 0.08184529114605077,
+          "rotation": 0,
+          "target": "10-bh-1_well-pad-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-bh-1_well-pad-4",
+      "name": "BH-1_Well Pad 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -1.6009195424913507,
+        "pitch": 0.12006021022855151,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.833677558102213,
+          "pitch": 0.10764658125372506,
+          "rotation": 0,
+          "target": "9-bh-1_well-pad-3"
+        },
+        {
+          "yaw": -1.5182741138022973,
+          "pitch": 0.11548447162975961,
+          "rotation": 0,
+          "target": "11-bh-1_cocrete-pad"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-bh-1_cocrete-pad",
+      "name": "BH-1_Cocrete Pad",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -0.17197268327666393,
+        "pitch": 0.38201466808585494,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.8101147498253116,
+          "pitch": 0.12787291496043984,
+          "rotation": 0,
+          "target": "10-bh-1_well-pad-4"
+        },
+        {
+          "yaw": 0.1125784651132804,
+          "pitch": -0.010232067597371497,
+          "rotation": 0,
+          "target": "2-bh-1_well-head-s"
+        },
+        {
+          "yaw": -0.09906955176940713,
+          "pitch": 0.019408856378731798,
+          "rotation": 0,
+          "target": "3-bh-1_well-head-w"
+        },
+        {
+          "yaw": -1.6787095516493515,
+          "pitch": 0.12346306403286,
+          "rotation": 0,
+          "target": "8-bh-1_well-pad-2"
+        },
+        {
+          "yaw": -0.06385816311690462,
+          "pitch": -0.20131082712206627,
+          "rotation": 0,
+          "target": "0-bh-1_overview-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-bh-1_well-pad-5",
+      "name": "BH-1_Well Pad 5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -2.915217310909231,
+        "pitch": 0.030098125364339978,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3885111933556473,
+          "pitch": 0.08025597845124466,
+          "rotation": 0,
+          "target": "6-bh-1_well-pad"
+        },
+        {
+          "yaw": 0.4943015261298136,
+          "pitch": 0.17379475052770665,
+          "rotation": 0,
+          "target": "13-bh-1_well-pad-6"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-bh-1_well-pad-6",
+      "name": "BH-1_Well Pad 6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 2.8256539325380956,
+        "pitch": 0.21607216539153917,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.303043737585364,
+          "pitch": 0.12970274870593101,
+          "rotation": 0,
+          "target": "12-bh-1_well-pad-5"
+        },
+        {
+          "yaw": 2.6391576738543447,
+          "pitch": 0.08289974813012257,
+          "rotation": 0,
+          "target": "14-bh-1_well-pad-7"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-bh-1_well-pad-7",
+      "name": "BH-1_Well Pad 7",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 2.1020567907208587,
+        "pitch": -0.10302819836255672,
+        "fov": 1.4032930175016776
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9163047843795615,
+          "pitch": 0.07669265511143308,
+          "rotation": 0,
+          "target": "13-bh-1_well-pad-6"
+        },
+        {
+          "yaw": 1.991909780308026,
+          "pitch": 0.008687581777502729,
+          "rotation": 0,
+          "target": "2-bh-1_well-head-s"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "WELL PAD BH-1",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
